a65e693311deca96646dbb5100278478d4105679
Dan Addison <danaddisoncreative@gmail.com>
2021-07-25 22:18:38 +0100

    Remove carousel lib, add simple horizontal scroll for project slides

--- wp-content/themes/da_boilerplate/assets/js/inits.js
+++ wp-content/themes/da_boilerplate/assets/js/inits.js
@@ -10,13 +10,4 @@ jQuery(function($) {
     }
   });
 
-
-    $('.owl-carousel').owlCarousel({
-        margin:10,
-        loop:true,
-        autoWidth:true,
-        items:2
-    });
-
-
 });
--- wp-content/themes/da_boilerplate/blocks/block-project-slides.php
+++ wp-content/themes/da_boilerplate/blocks/block-project-slides.php
@@ -3,7 +3,7 @@
 ?>
 
 <?php if( have_rows('slides_item') ): ?>
-<div class="block-project-slides owl-carousel owl-theme">
+<div class="block-project-slides">
 
     <?php while( have_rows('slides_item') ): the_row(); 
         $type = get_sub_field('acf_slide_media_type');
@@ -13,20 +13,16 @@
             $size = 'full';
         ?>
 
-            <div class="block-project-slides__item item">
-                <div class="block-project-slides__item-wrapper block-project-slides__item-wrapper--<?php echo $type ?>">
-                    <?php echo wp_get_attachment_image( $image, $size ); ?>
-                </div>
+            <div class="block-project-slides__item block-project-slides__item--<?php echo $type ?>">
+                <?php echo wp_get_attachment_image( $image, $size ); ?>
             </div>
 
         <?php
         } else {
             $video_url = get_sub_field('acf_slide_video');
         ?>
-            <div class="block-project-slides__item item">
-                <div class="block-project-slides__item-wrapper block-project-slides__item-wrapper--<?php echo $type ?>">
-                    <iframe src="<?php echo $video_url ?>"></iframe>
-                </div>
+            <div class="block-project-slides__item block-project-slides__item--<?php echo $type ?>">
+                <iframe src="<?php echo $video_url ?>"></iframe>
             </div> 
 
         <?php
