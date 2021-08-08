6a6e36c3ae53c7464292d7344474d41d8bceeda5
Dan Addison <danaddisoncreative@gmail.com>
2021-08-08 09:14:27 +0100

    Restrict available blocks, add GB colour options

--- wp-content/themes/da_boilerplate/functions/gutenberg-settings.php
+++ wp-content/themes/da_boilerplate/functions/gutenberg-settings.php
@@ -2,96 +2,66 @@
 
 // Conditionally allow specific Gutenberg blocks by post type
 function da_boilerplate_allowed_block_types( $allowed_blocks, $post ) {
+	
+    return array(
+        // common blocks
+        'core/paragraph',
+        'core/heading',
+        'core/list',
+        'core/image',
+        'core/gallery',
+        'core/quote',
+        'core/file',
+        // 'core/cover',
+        // 'core/audio',
+        // 'core/video',
+        
+        // formatting
+        'core/html', // eg. to embed iFrames
+        // 'core/table',
+        // 'core/pullquote',
+        // 'core/freeform', // classic editor block
+        'core/verse', // <pre> tag: includes text align options, eg. for poetry or song lyrics
+        // 'core/code', // <pre> tag: includes <code> element for specific styling eg. grey background colour
+        // 'core/preformatted', // <pre> tag: similar to code block but with more styling options
+        
+        // layout
+        'core/columns',
+        'core/separator',
+        // 'core/media-text',
+        'core/group',
+        // 'core/buttons',
+        'core/spacer', // extended for responsiveness in my editor.js file
+        // 'core/nextpage', // for adding page break (but doesn't produce pagination for me?)
 
-	// Return an array containing the allowed block types for 'post' post type
-	if($post->post_type == 'post') {	
-		return array(
-		// common blocks
-		'core/paragraph',
-		'core/heading',
-		'core/list',
-		'core/image',
-		'core/gallery',
-		'core/quote',
-		'core/file',
-		// 'core/cover',
-		// 'core/audio',
-		// 'core/video',
-		
-		// formatting
-		'core/html', // eg. to embed iFrames
-		'core/table',
-		'core/pullquote',
-		// 'core/freeform', // classic editor block
-		'core/verse', // <pre> tag: includes text align options, eg. for poetry or song lyrics
-		'core/code', // <pre> tag: includes <code> element for specific styling eg. grey background colour
-		'core/preformatted', // <pre> tag: similar to code block but with more styling options
-		
-		// layout
-		'core/columns',
-		'core/separator',
-		'core/media-text',
-		'core/group',
-		'core/buttons',
-		'core/spacer', // extended for responsiveness in my editor.js file
-		// 'core/nextpage', // for adding page break (but doesn't produce pagination for me?)
+        // widgets
+        // 'core/shortcode',
+        // 'core/search',
+        'core/social-links',
+        // 'core/latest-posts', // loads of options.. but probably never quite enough!
 
-		// widgets
-		'core/shortcode',
-		'core/search',
-		'core/social-links',
-		// 'core/latest-posts', // loads of options.. but probably never quite enough!
+        // embeds
+        // 'core-embed/twitter',
+        // 'core-embed/instagram',
+        'core-embed/youtube',
+        'core-embed/vimeo',
+        'core-embed/soundcloud',
+        // 'core-embed/facebook',
+        // 'core-embed/tiktok',
+        // 'core-embed/meetup-com',
 
-		// embeds
-		'core-embed/twitter',
-		'core-embed/instagram',
-		'core-embed/youtube',
-		'core-embed/vimeo',
-		'core-embed/soundcloud',
-		'core-embed/facebook',
-		'core-embed/tiktok',
-		'core-embed/meetup-com',
+        // reusable
+        'core/block',
 
-		// reusable
-		'core/block',
-
-		// third party
-		// 'wpforms/form-selector',
-		
-		// add our own blocks
-		'acf/accordion',
+        // third party
+        // 'wpforms/form-selector',
+        
+        // add our own blocks
+        'acf/accordion',
         'acf/project-list',
-        'acf/project-slides',
-		);
-	}
-	// Return an array containing the allowed block types for 'page' post type
-	elseif($post->post_type == 'page') {
-			return array(
-				// common blocks
-				'core/paragraph',
-				'core/heading',
-				'core/list',
-				'core/image',
-				'core/gallery',
-				'core/quote',
-				'core/file',
-				// 'core/cover',
-				// 'core/audio',
-				// 'core/video',
-
-				// reusable
-				'core/block',
-
-				// add our own blocks
-				'acf/hero-image',
-			);
-	}
-	// Allow defaults in all other post types
-	else {
-			return $allowed_block_types;
-	}
+    );
 }
-// add_filter( 'allowed_block_types', 'da_boilerplate_allowed_block_types', 10, 2 );
+add_filter( 'allowed_block_types', 'da_boilerplate_allowed_block_types', 10, 2 );
 
 /**
  * Add support for Block Styles.
@@ -156,24 +126,24 @@ function da_boilerplate_custom_editor_colour_palette() {
 	// Editor Color Palette
 	add_theme_support( 'editor-color-palette', array(
 		array(
-			'name'  => __( 'White', 'da_boilerplate' ),
-			'slug'  => 'white',
-			'color'	=> '#FFFFFF',
+			'name'  => __( 'Light', 'da_boilerplate' ),
+			'slug'  => 'light',
+			'color'	=> '#F7F7F2',
 		),
 		array(
-			'name'  => __( 'Black', 'da_boilerplate' ),
-			'slug'  => 'black',
-			'color'	=> '#000000',
+			'name'  => __( 'Dark', 'da_boilerplate' ),
+			'slug'  => 'dark',
+			'color'	=> '#222',
 		),
 		array(
-			'name'  => __( 'Grey', 'da_boilerplate' ),
-			'slug'  => 'grey',
-			'color'	=> '#bdbdbd',
+			'name'  => __( 'Middle', 'da_boilerplate' ),
+			'slug'  => 'middle',
+			'color'	=> '#d0d0c9',
 		),
 		array(
-			'name'  => __( 'Primary', 'da_boilerplate' ),
-			'slug'  => 'primary',
-			'color' => '#53c9c8'
+			'name'  => __( 'Accent', 'da_boilerplate' ),
+			'slug'  => 'accent',
+			'color' => '#778472'
 		),
 	) );
 }
